f3cf6643337a9a1fa84334076202bb2ac6c03189
Brian Anderson <banderson@mozilla.com>
2012-07-12 18:36:55 -0700

    Finish ConnectionFactory and mocks

--- connection.rs
+++ connection.rs
@@ -12,15 +12,15 @@ while letting us have additional implementations for
 mocking
 */
 trait Connection {
-    fn read_start() -> ReadPort;
+    fn read_start() -> result<ReadPort, tcp_err_data>;
     fn read_stop(-read_port: ReadPort) -> result<(), tcp_err_data>;
 }
 
-trait ConnectionFactory<C/*: connection*/> {
+trait ConnectionFactory<C: Connection> {
     fn connect(ip: ip_addr, port: uint) -> result<C, tcp_connect_err_data>;
 }
 
-impl Connection for tcp_socket {
+impl of Connection for tcp_socket {
     fn read_start() -> result<ReadPort, tcp_err_data> {
         import std::net::tcp::tcp_socket;
         self.read_start()
@@ -34,3 +34,36 @@ impl Connection for tcp_socket {
 
 type UvConnectionFactory = ();
 
+impl of ConnectionFactory<tcp_socket> for UvConnectionFactory {
+    fn connect(ip: ip_addr, port: uint) -> result<tcp_socket, tcp_connect_err_data> {
+        import std::uv_global_loop;
+        import std::net::tcp::connect;
+        let iotask = uv_global_loop::get();
+        ret connect(copy ip, port, iotask);
+    }
+}
+
+type MockConnection = {
+    read_start_fn: fn@() -> result<ReadPort, tcp_err_data>,
+    read_stop_fn: fn@(-ReadPort) -> result<(), tcp_err_data>
+};
+
+impl of Connection for MockConnection {
+    fn read_start() -> result<ReadPort, tcp_err_data> {
+        self.read_start_fn()
+    }
+
+    fn read_stop(-read_port: ReadPort) -> result<(), tcp_err_data> {
+        self.read_stop_fn(read_port)
+    }
+}
+
+type MockConnectionFactory = {
+    connect_fn: fn@(ip_addr, uint) -> result<MockConnection, tcp_connect_err_data>
+};
+
+impl of ConnectionFactory<MockConnection> for MockConnectionFactory {
+    fn connect(ip: ip_addr, port: uint) -> result<MockConnection, tcp_connect_err_data> {
+        self.connect_fn(ip, port)
+    }
+}
