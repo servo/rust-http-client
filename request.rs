bbf3d9dfc796d78548207a31361447fa57d9ddb7
Patrick Walton <pcwalton@mimiga.net>
2012-09-10 10:59:48 -0700

    Add query parameter support

--- request.rs
+++ request.rs
@@ -6,7 +6,16 @@ import std::net::url::Url;
 fn build_request(url: Url) -> ~str {
 
     let host = copy url.host;
-    let path = if url.path.is_not_empty() { copy url.path } else { ~"/" };
+    let mut path = if url.path.is_not_empty() { copy url.path } else { ~"/" };
+
+    if url.query.len() > 0 {
+        let kvps = do url.query.map |pair| {
+            match pair {
+                (key, value) => fmt!("%s=%s", key, value)
+            }
+        };
+        path += ~"?" + str::connect(kvps, "&");
+    }
 
     let request_header = #fmt("GET %s HTTP/1.0\u000D\u000AHost: %s\u000D\u000A\u000D\u000A",
                               path, host);
