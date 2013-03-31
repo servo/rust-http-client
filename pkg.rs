29cd8e0e0772a71474a70ae1e1541e2fd63bd9ec
Sean Billig <sean.billig@gmail.com>
2013-03-31 13:56:00 -0500

    Add rustpkg commands to build crate

--- pkg.rs
+++ pkg.rs
@@ -1,10 +1,13 @@
 #[pkg(id = "http_client",
       vers = "0.1.0")];
 
+extern mod rustpkg;
 use core::run;
 
 #[pkg_do(build)]
 fn main() {
     run::run_program("./configure", []);
     run::run_program("make", []);
+    let crate = rustpkg::Crate(~"http_client.rc");
+    rustpkg::build(~[crate]);
 }
